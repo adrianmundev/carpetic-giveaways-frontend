62a50916da4cc03529124417db087df2ef424e33
ibrarmunircoder <ibrarmunir009@gmail.com>
2024-02-02 21:55:23 +0500

    fixed date issue

--- components/cards/ContestCard.tsx
+++ components/cards/ContestCard.tsx
@@ -17,9 +17,17 @@ const ContestCard: React.FC<ContestCardProps> = ({ product }) => {
 
   const displayRemainingDays = (drawDate: Date) => {
     const now = dayjs();
-    const rewardDate = dayjs(drawDate);
-    const days = rewardDate.diff(now, "days");
-    if (days < 0) {
+    const oneDayLater = now
+      .add(1, "days")
+      .set("hours", 23)
+      .set("minutes", 59)
+      .set("seconds", 59)
+      .set("milliseconds", 999);
+
+    const rewardDate = dayjs(product.drawDate);
+    const days = Math.ceil(rewardDate.diff(now, "days", true));
+
+    if (oneDayLater.isAfter(rewardDate)) {
       return null;
     }
 
@@ -28,27 +36,29 @@ const ContestCard: React.FC<ContestCardProps> = ({ product }) => {
 
   const displayDrawingSoonTag = () => {
     const now = dayjs();
+    const oneDayLater = now.add(1, "days");
+
     const rewardDate = dayjs(product.drawDate);
-    const days = rewardDate.diff(now, "days");
-    if (days > 1) {
-      return null;
+
+    if (oneDayLater.isAfter(rewardDate)) {
+      return (
+        <div className="tw-absolute tw-bottom-0 tw-left-0 tw-flex tw-h-9">
+          <div className="tw-flex tw-min-w-[180px] tw-items-center tw-bg-SelectiveYellow tw-px-4 tw-font-bold tw-text-white">
+            <div className="tw-flex tw-h-screen tw-items-center tw-justify-center">
+              <div className="tw-relative tw-mr-2 tw-inline-flex">
+                <div className="tw-h-2 tw-w-2 tw-rounded-full tw-bg-white"></div>
+                <div className="tw-absolute tw-left-0 tw-top-0 tw-h-2 tw-w-2 tw-animate-ping tw-rounded-full tw-bg-white"></div>
+                <div className="tw-absolute tw-left-0 tw-top-0 tw-h-2 tw-w-2 tw-animate-pulse tw-rounded-full tw-bg-white"></div>
+              </div>
+            </div>
+            Closes Tomorrow {dayjs(product.drawDate).format("hh:mm")}
+          </div>
+          <div className="tw-w-0 tw-border-r-[22px] tw-border-t-[36px] tw-border-transparent tw-border-t-SelectiveYellow" />
+        </div>
+      );
     }
 
-    return (
-      <div className="tw-absolute tw-bottom-0 tw-left-0 tw-flex tw-h-9">
-        <div className="tw-flex tw-min-w-[180px] tw-items-center tw-bg-SelectiveYellow tw-px-4 tw-font-bold tw-text-white">
-          <div className="tw-flex tw-h-screen tw-items-center tw-justify-center">
-            <div className="tw-relative tw-mr-2 tw-inline-flex">
-              <div className="tw-h-2 tw-w-2 tw-rounded-full tw-bg-white"></div>
-              <div className="tw-absolute tw-left-0 tw-top-0 tw-h-2 tw-w-2 tw-animate-ping tw-rounded-full tw-bg-white"></div>
-              <div className="tw-absolute tw-left-0 tw-top-0 tw-h-2 tw-w-2 tw-animate-pulse tw-rounded-full tw-bg-white"></div>
-            </div>
-          </div>
-          Closes Tomorrow {dayjs(product.drawDate).format("hh:mm")}
-        </div>
-        <div className="tw-w-0 tw-border-r-[22px] tw-border-t-[36px] tw-border-transparent tw-border-t-SelectiveYellow" />
-      </div>
-    );
+    return null;
   };
 
   const thumbnail = product.thumbnailUrl || product.images[0]?.imageUrl;
